a1771a807ddf7f1c9d3ea47fa9e548858f520c59
Sheryyx <pubggamee736@gmail.com>
2023-08-19 17:36:45 +0500

    dusra number

---
+++ 02_Variables.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5,b=6;
+    char var  = 'c';
+    float f = 3.14;
+    double sam = 3.144564634;
+    cout<<"the val of a is: "<<a<<endl<<"the val of b: "<<b<<endl;
+    cout<<"the val of f is: "<<f<<endl<<"the val of sam: "<<sam<<endl;
+    int num1,num2;
+    char var1;
+    float f1;
+    cout<<"enter the value of num1: "<<endl;
+    cin>>num1;
+    cout<<"enter the value of num2: "<<endl;
+    cin>>num2;
+    cout<<"enter the value of var1: "<<endl;
+    cin>>var1;
+    cout<<"enter the value of float : "<<endl;
+    cin>>f1;
+    cout<<"the sum of num 1 and 2 is: "<<num1+num2<<"\ncharacter is: "<<var1<<"\nfloat value is: "<<f1;
+
+   
+
+return 0;
+}
